7bb7ffaee68b9eb3d60246be9057534de2800d18
Marlos Augusto <marlos.gomes@codengage.com>
2019-11-01 21:51:48 -0300

    fix(firebase): fix firebase config

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import App from './App';
 import firebase from "@firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBeT2Rn5oOE_JgGoF-APeM801E6EscSLZY",
-  authDomain: "projetoutfpr-appantigo.firebaseapp.com",
-  databaseURL: "https://projetoutfpr-appantigo.firebaseio.com",
-  projectId: "projetoutfpr-appantigo",
-  storageBucket: "projetoutfpr-appantigo.appspot.com",
-  messagingSenderId: "1090604264681",
-  appId: "1:1090604264681:web:d4b888bd5505e2c492d478"
+  apiKey: "AIzaSyCTGh1TGOdAGPOAwgvX-KYro3ZmUyh65vA",
+  authDomain: "receitas-web2react.firebaseapp.com",
+  databaseURL: "https://receitas-web2react.firebaseio.com",
+  projectId: "receitas-web2react",
+  storageBucket: "receitas-web2react.appspot.com",
+  messagingSenderId: "592954266495",
+  appId: "1:592954266495:web:1a4555e0c528c9b3dd404a"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
