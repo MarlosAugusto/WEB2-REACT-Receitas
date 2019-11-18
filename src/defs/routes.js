38aceb9a647ac9bf2da9ec7701accfbf92883e81
MarlosAugusto <marlos.gomes@codengage.com>
2019-11-17 23:01:42 -0200

    feat(routes): add routes file

---
+++ src/defs/routes.js
@@ -0,0 +1,7 @@
+export const routes = [
+  { text: 'Home', url: '/' },
+  { text: 'Receitas Doces', url: '/receitas-doces' },
+  { text: 'Receitas Salgadas', url: '/receitas-salgadas' },
+  { text: 'Nova Receita', url: '/nova-receita' },
+  { text: 'Contato', url: '/contato' },
+]
