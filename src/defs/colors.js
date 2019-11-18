93e8d75be7156c7137b3a59df1ba06246b1df55b
MarlosAugusto <marlos.gomes@codengage.com>
2019-11-17 22:57:48 -0200

    feat(defs): set primary and secundary colors

--- src/defs/colors.js
+++ src/defs/colors.js
@@ -1,1 +1,10 @@
-export const primary = "#ff0000";
+export const colors = {
+  primary: "#4c20a1",
+  primary50: "#4c20a180",
+  primaryDark: "#0f0326",
+  secundary: "#eb703d",
+  secundary50: "#eb703d80",
+  secundaryDark: "#61270f",
+}
+
+export default colors
