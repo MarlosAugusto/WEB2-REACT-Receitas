987b93ebe64bac0c2428ff47927668fe92259540
MarlosAugusto <marlos.gomes@codengage.com>
2019-11-17 22:58:56 -0200

    feat(component): header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,50 @@
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-export const Header = styled.div`
+import "../assets/fonts/Malvinas Sans/style.css";
+import colors from "../defs/colors";
+
+const HD = styled.div`
   width: 100%;
   height: 80px;
-  background-color: #00000099;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  background-color: ${colors.primaryDark};
 `;
+
+const Logo = styled.img`
+  height: 80%;
+  width: auto;
+`;
+
+const Menu = styled.div`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const MenuItem = styled.a`
+  margin-left: 20px;
+  text-decoration: none;
+  font-family: "Malvinas Regular Regular";
+  color: ${colors.secundary};
+
+  :hover {
+    text-decoration: underline;
+    color: ${colors.secundary50};
+  }
+`;
+
+export const Header = ({ logo, submenus }) => (
+  <HD>
+    {console.log("submenus", submenus)}
+    <Logo src={logo} />
+    <Menu>
+      {submenus.map(({ text, url }) => (
+        <MenuItem href={url}>{text}</MenuItem>
+      ))}
+    </Menu>
+  </HD>
+);
