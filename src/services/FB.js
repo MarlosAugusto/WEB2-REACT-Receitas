6eef50888be103e15efd4ed3503f23bc01b5bb08
MarlosAugusto <marlos.gomes@codengage.com>
2019-11-21 09:34:38 -0200

    feat(firebase): GET and POST files

--- src/services/FB.js
+++ src/services/FB.js
@@ -1,6 +1,7 @@
 import firebase from "@firebase/app";
 import "@firebase/auth";
 import "@firebase/database";
+import "@firebase/storage";
 
 export const GET = async (path) => {
   let data;
@@ -22,6 +23,25 @@ export const POST = async (data, path) => {
     .push(data)
     .then(() => (success = true));
   return success;
+};
+
+export const POSTFILE = async (type, file, filename) => {
+  const storageRef = firebase.storage().ref();
+  const fileRef = storageRef.child(`${type}/${filename}.jpg`);
+  let success = false;
+  await fileRef.put(file).then(() =>
+    success = true
+  );
+  return success;
+};
+
+export const GETFILE = async (type, filename) => {
+  const storageRef = firebase.storage().ref()
+  const fileRef = storageRef.child(`${type}/${filename}.jpg`)
+  if (!fileRef) {
+    return false
+  }
+  return await fileRef.getDownloadURL()
 };
 
 export const PUT = async (data, path) => {
